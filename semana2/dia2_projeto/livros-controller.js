991217899f81fb7774d0afac3dc9b7e642f086c3
José Satiro <jardelsatiro57@gmail.com>
2025-07-06 19:27:20 -0300

    .

--- semana2/dia2_projeto/livros-controller.js
+++ semana2/dia2_projeto/livros-controller.js
@@ -1,20 +1,56 @@
-import { conexao } from './conexao.js';
+// import { conexao } from './conexao.js';
 
-export const livrosCadastrados = async () => {
-    const [criarTabela] = await conexao.execute(
-        ` create table if not exists livros (
-            id int primary key auto_increment,
-            titulo varchar(150) not null,
-            autor varchar(150) not null,
-            ano date not null,
-            genero varchar(80) not null,
-            quantidade_estoque int not null
-        )`
-    )
-    const [tabela] = await conexao.execute(
-        'select * from livros'
-    )
-    return tabela
-}
+// export const livrosCadastrados = async () => {
+//     const [criarTabela] = await conexao.execute(
+//         ` create table if not exists livros (
+//             id int primary key auto_increment,
+//             titulo varchar(150) not null,
+//             autor varchar(150) not null,
+//             ano date not null,
+//             genero varchar(80) not null,
+//             quantidade_estoque int not null
+//         )`
+//     )
+//     const [tabela] = await conexao.execute(
+//         'select * from livros'
+//     )
+//     return tabela
+// }
 
-console.log(await livrosCadastrados())
+// export const listarLivros = async(livros) => {
+
+// }
+
+let num = 1.12
+console.log(parseInt(num))
+
+// 💡 Desafio: Sistema de Gerenciamento de Livros (Back-End com Express + MySQL)
+
+
+// ---
+
+// 📌 Requisitos principais:
+
+// 1. Banco de Dados: -> feito
+
+// 2. Endpoints obrigatórios (Express):
+
+// GET /livros: lista todos os livros ou filtra por título, autor, ano ou gênero (usando query params).
+
+// GET /livros/:id: mostra os dados de um único livro.
+
+// POST /livros: cadastra um novo livro.
+
+// PUT /livros/:id: atualiza qualquer informação de um livro.
+
+// DELETE /livros/:id: remove o livro do sistema.
+
+
+
+// 3. Extras para nível hard (opcional):
+
+// Adicione um campo disponível e altere automaticamente para false se quantidade_estoque for 0.
+
+// Faça validações no cadastro (campos obrigatórios, ano numérico, etc).
+
+// Ordenação por ano ou título (?ordenar=ano ou ?ordenar=titulo).
