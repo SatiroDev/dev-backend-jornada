a68e027f5c22549df6dadd77dfb363307b2d04ef
José Satiro <jardelsatiro57@gmail.com>
2025-07-06 19:27:24 -0300

    Update index.js

--- semana2/dia2_projeto/index.js
+++ semana2/dia2_projeto/index.js
@@ -1,8 +1,39 @@
 import express from 'express'
-import { atualizarNome, atualizarSenha, mostrarUsuarios, mostrarUsuario, verificarNome, criarUsuario, verificarNomeSenha } from "./auth-controller.js";
-// auth/index.js
-const usuarios = await mostrarUsuarios()
+import { livrosCadastrados } from "./livros-controller.js";
 
 const app = express()
 
 const port = 3000
+
+app.get('/livros', async (req, res) => {
+    let listaLivros = livrosCadastrados()
+    const {titulo, autor, ano, genero} = req.query
+    titulo = titulo.toString()
+    autor = autor.toString()
+    ano = parseInt(ano)
+    genero = genero.toString()
+    if (titulo) {
+        listaLivros = listaLivros.filter(titulo => l.titulo === titulo)
+    }
+
+    if (autor) {
+        listaLivros = listaLivros.filter(autor => l.listaLivros === autor)
+    }
+
+    if (ano) {
+        listaLivros = listaLivros.filter(ano => l.listaLivros === ano)
+    }
+
+    if (genero) {
+        listaLivros = listaLivros.filter(genero => l.listaLivros === genero)
+    }
+
+    res.json(listaLivros)
+})
+
+app.listen(port, async () => {
+    console.log('Servidor rodando na porta -> ', port)
+
+})
+
+//GET /livros: lista todos os livros ou filtra por título, autor, ano ou gênero (usando query params).
